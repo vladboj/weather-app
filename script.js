554101535035909485bc678bc967223031f9a0c0
vladboj <vlad.bojan0@gmail.com>
2023-12-19 16:31:33 +0200

    add input handling to get weather data

---
+++ script.js
@@ -0,0 +1,28 @@
+// key: d3f843a6b7a4493e94c162544231812
+
+const input = document.querySelector("input");
+
+async function requestData(url) {
+  try {
+    const rawData = await fetch(url);
+    const data = await rawData.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+input.addEventListener("keydown", async (event) => {
+  if (event.code === "Enter") {
+    const city = input.value.toLowerCase();
+    input.value = "";
+
+    const url = `http://api.weatherapi.com/v1/current.json?key=d3f843a6b7a4493e94c162544231812&q=${city}`;
+
+    const data = await requestData(url);
+
+    const output = document.querySelector("div.output");
+    console.log(data);
+    output.innerHTML = data.location.name + " " + data.current.temp_c;
+  }
+});
